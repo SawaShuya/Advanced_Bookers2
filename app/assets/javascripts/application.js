f3c6e4e4014e73a0492d558889fd95ab825be48f
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2021-04-09 00:14:08 +0900

    image preview

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,6 +36,22 @@ $(function() {
   });
 });
 
+$(function () {
+  $(document).on('turbolinks:load', function () {
+    function readURL(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $('.img_prev').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
+    }
 
+    $('.user_image').change(function () {
+      readURL(this);
+    });
+  });
+});
 
 
